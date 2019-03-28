c3507aaebd51e15684c4ef5930b0562dd5a6ed74
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-03-28 14:01:38 +0100

    ✨ Added .localStorage() for color and text

--- opdracht-2/public/js/script.js
+++ opdracht-2/public/js/script.js
@@ -1,1 +1,34 @@
+(() => {
 
+  const form = document.querySelector('form')
+
+  if (form) {
+    form.onsubmit = saveToLocalStorage
+  }
+
+  function saveToLocalStorage() {
+    const radioButtons = document.getElementsByName('kleur'),
+          input = document.getElementById('bt-text')
+
+    radioButtons.forEach( x => {
+        if ( x.checked === true ) {
+          console.log(x)
+          localStorage.setItem('color', JSON.stringify(x.value))
+        }
+    })
+
+    localStorage.setItem('text', JSON.stringify(input.value))
+  }
+
+  function getLocalStorageData() {
+    const svg = document.getElementsByClassName('fff611fd-d11e-429f-bd57-f749dd6baa13')
+
+    let color = localStorage.getItem('color'),
+        text = localStorage.getItem('text')
+
+    svg[0].style.fill = JSON.parse(color);
+  }
+
+  getLocalStorageData()
+
+})()
